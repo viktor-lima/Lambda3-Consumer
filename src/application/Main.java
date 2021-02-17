dc2abb3fbb95f0035db1b4883bcf7951d5ae024b
viktor-lima <joaolimak10@outlook.com>
2021-02-17 18:00:18 -0300

    Declared lambda espression

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 import entities.Product;
@@ -19,6 +20,9 @@ public class Main {
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
+		
+		Consumer<Product> cons = p -> p.setPrice(p.getPrice() * 1.1);
+		
 		
 		list.forEach(Product::nonPriceStaticUpdate);
 		
