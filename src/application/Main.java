5fa42b20b742d1e40639e199612c2983be05d6f4
viktor-lima <joaolimak10@outlook.com>
2021-02-17 18:03:51 -0300

    Declared lambda expression (v)

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,10 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Predicate;
+import java.util.function.Consumer;
 
 import entities.Product;
-import utils.UpdatePrice;
 
 public class Main {
 	public static void main(String[] args) {
@@ -20,7 +19,10 @@ public class Main {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(Product::nonPriceStaticUpdate);
+		Consumer<Product> cons = p -> p.setPrice(p.getPrice() * 1.1);
+		
+		
+		list.forEach(cons);
 		
 		list.forEach(System.out::println);
 		
