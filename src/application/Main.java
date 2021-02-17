c67bd2ef0638e3f1e635c649270d6cbdb3a0a79c
viktor-lima <joaolimak10@outlook.com>
2021-02-17 17:46:11 -0300

    first commit

---
+++ src/application/Main.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Predicate;
+
+import entities.Product;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		double min = 100;
+		
+		list.removeIf( p -> p.getPrice() >= min);
+		
+		
+		for (Product p : list) {
+			System.out.println(p);
+		}
+		
+		
+	}
+}
